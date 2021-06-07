b782094fc19c189fdcae580c644c91c72afc7646
shubham3211 <kumarsinghshubham32111@gmail.com>
2021-06-07 09:23:29 +0530

    error handler added

--- middlewares/validateJwt.js
+++ middlewares/validateJwt.js
@@ -2,16 +2,22 @@ const { verifyAccessToken } = require("../services/tokenService");
 const httpStatus = require("http-status");
 
 const validateJwt = async (req, res, next) => {
-  if (!req.headers["authorization"]) {
-    res.status(httpStatus.UNAUTHORIZED).send({ message: "token required" });
+  try {
+    if (!req.headers["authorization"]) {
+      res.status(httpStatus.UNAUTHORIZED).send({ message: "token required" });
+    }
+    const token = req.headers["authorization"].split(" ")[1];
+    const payload = await verifyAccessToken(token);
+    if (!req.locals) {
+      req.locals = {};
+    }
+    req.locals.user = payload;
+    next();
+  } catch (err) {
+    res.status(httpStatus.UNAUTHORIZED).send({
+      message: err.message,
+    });
   }
-  const token = req.headers["authorization"].split(" ")[1];
-  const payload = await verifyAccessToken(token);
-  if (!req.locals) {
-    req.locals = {};
-  }
-  req.locals.user = payload;
-  next();
 };
 
 module.exports = { validateJwt };
